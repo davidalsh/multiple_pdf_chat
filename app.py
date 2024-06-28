5421f44c5c31614ac05a9786bd31b5c39ca31fb2
davidalshevski <software.dev.davidalsh@gmail.com>
2024-06-28 21:27:13 +0200

    [+] UI, extracting pdf text logic

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 
 def main():
@@ -10,8 +20,11 @@ def main():
     st.text_input("Ask a question about your documents:")
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click on 'Process'")
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
 
 
 if __name__ == "__main__":
